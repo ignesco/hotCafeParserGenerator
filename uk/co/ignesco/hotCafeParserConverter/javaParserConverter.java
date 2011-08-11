b24e88c48373d1e13c1cac5b359e357c501f92a7
CKH <chime@ventrica>
2011-08-11 12:08:31 +0100

    added code to enable PMD warnings to be handled

--- uk/co/ignesco/hotCafeParserConverter/javaParserConverter.java
+++ uk/co/ignesco/hotCafeParserConverter/javaParserConverter.java
@@ -10,7 +10,24 @@ public class javaParserConverter
     {
 	if(s.length==3)
 	    {
-		javaParserConverter jc = new javaParserConverter(s[1],s[2]);
+		javaParserConverter jc = new javaParserConverter(s[1],s[2],"parserTableInitialiser","parserTableInit","non",null);
+		jc.process(s[0]);
+	    }
+        else if(s.length>=6)
+	    {
+                String[] imports = null;
+                if(s.length>6)
+                {
+                    int numberOfImports = s.length - 6;
+                    imports = new String[numberOfImports];
+                    
+                    for(int i=0;i<numberOfImports;i++)
+                    {
+                        imports[i] = s[6+i];
+                    }
+                }
+                
+		javaParserConverter jc = new javaParserConverter(s[1],s[2],s[3],s[4],s[5],imports);
 		jc.process(s[0]);
 	    }
 	else
@@ -19,13 +36,52 @@ public class javaParserConverter
 
     static void usage()
     {
-	System.out.println("Usage : javaParserConverter parserTable.h package_name class_name");
+	System.out.println("Usage : javaParserConverter parserTable.h package_name class_name [parserTableInitialiser-classname parserTableInit-classname throws_class_use_DASH_if_non_is_needed optional-imports]");
+        System.out.println("Usage : You can change the function names that are output using the following system properties -DSHIFT_ACTION=X -DREDUCE_ACTION=X -DGOTO_ENTRY=X -DEND_OF_TOKENS_ACTION_ACCEPT=X");
     }
 
-    public javaParserConverter(String packageName,String className)
+    public javaParserConverter(String packageName,String className,String parserTableInitialiserClassName,String parserTableInitClassName,String throwsClass,String []imports)
     {
 	this.packageName = packageName;
 	this.className = className;
+        this.parserTableInitialiserClassName = parserTableInitialiserClassName;
+        this.parserTableInitClassName = parserTableInitClassName;
+        this.throwsClass = throwsClass;
+        this.supressWarnings = System.getProperty("SUPRESS_WARNINGS",null);
+        this.imports = imports;
+        
+        
+        origFunc = new String[5];
+        newFunc = new String[5];
+        
+        origFunc[0] = "SHIFT_ACTION";
+        origFunc[1] = "REDUCE_ACTION_ON_END_OF_TOKENS";
+        origFunc[2] = "REDUCE_ACTION";
+        origFunc[3] = "GOTO_ENTRY";
+        origFunc[4] = "END_OF_TOKENS_ACTION_ACCEPT";
+        
+        for(int i=0;i<origFunc.length;i++)
+            newFunc[i] = System.getProperty(origFunc[i], origFunc[i]);
+    }
+    
+    public String getOutputFunctionName(String s)
+    {
+        int index = -1;
+        
+        if(s.contains("SHIFT_ACTION"))
+            index = 0;
+        else if(s.contains("REDUCE_ACTION_ON_END_OF_TOKENS"))
+            index = 1;
+        else if(s.contains("REDUCE_ACTION"))
+            index = 2;
+        else if(s.contains("GOTO_ENTRY"))
+            index = 3;
+        else if(s.contains("END_OF_TOKENS_ACTION_ACCEPT"))
+            index = 4;
+        
+        String newStr = s.replace(origFunc[index],newFunc[index]);
+        
+        return newStr;
     }
 
     public void process(String filename)
@@ -37,9 +93,25 @@ public class javaParserConverter
 		     public void fileStart()
 		     {
 			 System.out.println("package "+packageName+";");
-			 System.out.println("import uk.co.ignesco.hotCafeParser.*;");
-			 System.out.println("public class "+className+" implements parserTableInitialiser{");
-			 System.out.println("public "+className+"(){\n}\n\n\tpublic void doInit(parserTableInit obj)\n\t{\n");
+                         
+                         if(imports==null)
+                            System.out.println("import uk.co.ignesco.hotCafeParser.*;");
+                        else
+                        {
+                            for(int i=0;i<imports.length;i++)
+                            {
+                                System.out.println("import "+imports[i]+";");
+                            }
+                        }
+                         
+                         if(supressWarnings!=null) {
+                            System.out.println("@SuppressWarnings("+supressWarnings+")");
+                         }
+                         
+			 System.out.println("public class "+className+" implements "+parserTableInitialiserClassName+"{");
+			 System.out.println("public "+className+"(){\n}\n\n\tpublic void doInit("+parserTableInitClassName+" obj)"+
+                                 (!throwsClass.equals("non")?" throws "+throwsClass:"")
+                                 + "\n\t{\n");
 		     }
 		     public void line(String s)
 		     {
@@ -49,13 +121,11 @@ public class javaParserConverter
 			    s.contains("REDUCE_ACTION") ||
 			    s.contains("GOTO_ENTRY") ||
 			    s.contains("END_OF_TOKENS_ACTION_ACCEPT")
-
-
 			    )
 			     outputObj = true;
 
 			 if(outputObj)
-			     System.out.println("obj."+s+";");
+			     System.out.println("obj."+getOutputFunctionName(s)+";");
 			 else 
 			     System.out.println(s);
 		     }
@@ -69,6 +139,13 @@ public class javaParserConverter
 
     String packageName;
     String className;
+    String parserTableInitialiserClassName;
+    String parserTableInitClassName;
+    String throwsClass;
+    String supressWarnings;
+    String []imports;
+    String []origFunc;
+    String []newFunc;
 }
 
 class textFileIterator
